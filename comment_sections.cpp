c5a610b0d9e19e141c1b3e8b245c145da98c24c0
Sami Sorell <sami.sorell@iki.fi>
2022-01-20 19:23:50 +0200

    Add comment sections.

---
+++ comment_sections.cpp
@@ -0,0 +1,19 @@
+/*----- Class -----------------------------------------------------------------
+ * Accurate delta values for R,G,B calculated from difference of two colors and
+ * divided for equal steps.
+ * The delta accuracy is 1/128 of a pixel color accuracy.
+ *----------------------------------------------------------------------------*/
+class LeClass;
+
+/*----- Function --------------------------------------------------------------
+ * Does:
+ *   This function does awesome and beautiful things.
+ *
+ * Wants:
+ *   param1: Le param.
+ *   param1: La param.
+ *
+ * Gives:
+ *   Product of les params.
+ *----------------------------------------------------------------------------*/
+int leFunction(int param1, int param2);
